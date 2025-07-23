0a3460fbe4aca03550162f3e0369781ab29ac7a7
AhmedRefaat00 <ahmedrefaat00ar@gmail.com>
2025-07-23 22:05:21 +0300

    last commit before merge with main

--- src/screens/Explore.js
+++ src/screens/Explore.js
@@ -1,6 +1,8 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { LeafletView } from 'react-native-leaflet-view';
+import '../../assets/leaflet.html'
+
 
 const Explore = () => {
 
@@ -8,12 +10,32 @@ const Explore = () => {
 
     return (
       <LeafletView
-        mapCenterPosition={{
-          lat: DEFAULT_LOCATION.latitude,
-          lng: DEFAULT_LOCATION.longitude,
-        }}
-      />
-    );
+          mapCenterPosition={{
+            lat: DEFAULT_LOCATION.latitude,
+            lng: DEFAULT_LOCATION.longitude,
+          }}
+          mapLayers={[
+            {
+              baseLayer: true,
+              url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+              minNativeZoom: 11,
+              maxNativeZoom: 18,
+              minZoom: 11,
+              maxZoom: 20,
+            },
+          ]}
+          mapMarkers={[
+            {
+              id: 'my-marker',
+              position: { lat: DEFAULT_LOCATION.latitude, lng: DEFAULT_LOCATION.longitude },
+              icon: 'https://cdn-icons-png.flaticon.com/64/2776/2776067.png',
+              size: [64, 64],
+              iconAnchor: [32, 64],
+            },
+          ]}
+          onMessage={(message) => console.log('Message from Leaflet:', message)}
+        />
+      );
   };
 
 export default Explore
